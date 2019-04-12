6ff6dc4fd0d34e6691fbab41c1f3d34c5a4e6062
admin <admin@admindeMacBook-Pro.local>
2019-04-12 21:39:28 +0800

    AtomicReference<V>
    AtomicReference<V>

---
+++ Thread/atomic/AtomicReference<V>/AtomicReference<V>/updateAndGet(UnaryOperator<V> updateFunction)/Hello/src/lab/Person.java
@@ -0,0 +1,51 @@
+package lab;
+
+/**
+ * 人
+ */
+public class Person {
+
+    /**
+     * 姓名
+     */
+    private String name;
+
+    /**
+     * 年龄
+     */
+    private int age;
+
+    /**
+     * 无参构造方法
+     */
+    public Person() {
+
+    }
+
+    /**
+     * 有参构造方法
+     *
+     * @param name 姓名
+     * @param age  年龄
+     */
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
