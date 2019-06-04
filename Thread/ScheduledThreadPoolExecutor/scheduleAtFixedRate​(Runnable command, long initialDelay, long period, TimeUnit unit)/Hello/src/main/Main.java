bee25450591f849955197c1a4eab4c75891883f8
admin <admin@admindeMacBook-Pro.local>
2019-06-04 09:43:38 +0800

    一次性任务与周期性任务
    一次性任务与周期性任务

---
+++ Thread/ScheduledThreadPoolExecutor/scheduleAtFixedRate​(Runnable command, long initialDelay, long period, TimeUnit unit)/Hello/src/main/Main.java
@@ -0,0 +1,34 @@
+package main;
+
+import java.util.concurrent.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // 创建执行定时任务的线程池
+        ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(5);
+        // 创建任务
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // 使当前执行任务的线程睡1秒
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("你好，周期性任务！");
+            }
+        };
+        // 提交延时任务
+        scheduledThreadPool.scheduleAtFixedRate(runnable, 3, 1, TimeUnit.SECONDS);
+        try {
+            // 使主线程睡10秒钟
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        // 关闭线程池
+        scheduledThreadPool.shutdown();
+    }
+}
